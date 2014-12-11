64a6b1749b278cac3fa497ec9b57504007143e1a
Martin Lazarov <martin@lazarov.bg>
2014-12-11 15:55:11 +0000

    Implementing basic user function

--- modules/users.js
+++ modules/users.js
@@ -5,13 +5,36 @@ var db = require('monk')('localhost/snapp');
 var users = db.get('user');
 
 exports.autoLogin = function(user, pass, callback){
+	user.findOne({username:user}, function(err, doc) {
+		if (doc){
+			doc.password == pass ? callback(doc) : callback(null);
+		} else{
+			callback(null);
+		}
+	});
 
 }
 
 exports.Login = function(user, pass, callback){
+	user.findOne({username:user,password:pass}, function(err, doc) {
+		if (doc){
+			callback(false,doc);
+		} else{
+			callback('Invalid username or password');
+		}
+	});
 
 }
 
 exports.Register = function(user, pass, callback){
-
+	// TODO: add password encription
+	// First check if that users exits
+	users.findOne({username:user}, function(err, doc) {
+		if(doc){
+			callback('User already exists');
+		}else{
+			// Create new account 
+			accounts.insert({username:user,password:pass}, {safe: true}, callback);
+		}
+	});
 }
